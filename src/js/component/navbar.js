9b17b879f2d5cf626b90491c6971ac67fbf66ddd
JuliOli-iu <olivera.julieta1912@gmail.com>
2024-11-06 16:06:51 +0000

    mas estilos :lipstick

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light shadow-sm py-3 px-4 mb-3">
+		<nav className="navbar navbar-light bg-light shadow-sm  px-4 mb-3">
 			<Link to="/">
-				<span className="p-2 navbar-brand mb-0 h1 fas fa-address-book mr-2"> Agenda de ju </span>
+				<span className="p-2 navbar-brand mb-0 mr-2"> 📓 Agenda de ju 😸</span>
 			</Link>
 			<div className="ml-auto btn">
 				<Link to="/create">

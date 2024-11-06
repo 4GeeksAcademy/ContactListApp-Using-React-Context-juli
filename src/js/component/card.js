7ef77c9c3d45cdcb1146225e69ac826425431325
JuliOli-iu <olivera.julieta1912@gmail.com>
2024-11-06 22:27:59 +0000

    arreglos

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,35 +1,33 @@
-import React, { useContext }from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext"
 
 export const ContactCard = ({ id, name, image, email, phone, address, edit }) => {
-const {actions} = useContext(Context)
+    const { actions } = useContext(Context)
 
     return (
-        <div className="card mb-4 shadow-sm rounded" style={{ maxWidth: "1000px" }}>
+        <div className="card mb-3" style={{ maxWidth: "1000px", minHeight: "150px" }}>
             <div className="row g-0">
-                <div className="col-md-4 ">
-                    <img src={image} className="img-fluid rounded-circle ms-6 mt-1" alt={name} style={{
+                <div className="col-md-4">
+                    <img src={image} className="img-fluid rounded-circle p-2" alt={name} style={{
                         width: "195px",
                         height: "195px",
                     }} />
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                         <div className="d-flex justify-content-between align-items-center">
+                        <div className="d-flex justify-content-between">
                             <h5 className="card-title">{name}</h5>
-
-                            <div className="d-flex">
-
-                                <button type="button" onClick={() => edit()} className="m-2 p-2 btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#editModal">
-                                <i className="fa fa-pen mx-1 fs-5"></i>
+                            <div className="d-flex mb-3">
+                                <button type="button" onClick={() => edit()} className="p-2 btn" data-bs-toggle="modal" data-bs-target="#editModal">
+                                    <i className="fa fa-pen mx-2 fs-4"></i>
                                 </button>
 
 
-                                <button type="button" className="m-2 p-2 btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                <i className="fa fa-trash mx-1 fs-5"></i>
+                                <button type="button" className="p-2 btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    <i className="fa fa-trash fs-4 text-danger"></i>
                                 </button>
 
-                                <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div className="modal-dialog">
                                         <div className="modal-content">
                                             <div className="modal-header">
@@ -37,25 +35,24 @@ const {actions} = useContext(Context)
                                                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div className="modal-body">
-                                                ? desea borrar a {name}
+                                            ¿Deseas borrar a {name}?
                                             </div>
                                             <div className="modal-footer">
                                                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                <button type="button" onClick = {() => actions.deleteContacts(id)} className="btn btn-danger" data-bs-dismiss="modal">Delete</button>
+                                                <button type="button" onClick={() => actions.deleteContacts(id)} className="btn btn-danger" data-bs-dismiss="modal">Delete</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <p className="card-text"> <i className="fa fa-phone me-2"> </i> Phone Number: {phone}</p>
-                        <p className="card-text"> <i className="fa fa-envelope me-2"></i> Email: {email}</p>
-                        <p className="card-text"> <i className="fa fa-map-pin me-2"></i> Address: {address}</p>
+                        <p className="card-text"> <i className="fa fa-phone"> </i> Phone Number: {phone}</p>
+                        <p className="card-text"> <i className="fa fa-envelope"></i> Email: {email}</p>
+                        <p className="card-text"> <i className="fa fa-map-pin"></i> Address: {address}</p>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
-
 

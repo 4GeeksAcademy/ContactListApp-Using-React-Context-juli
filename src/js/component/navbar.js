0f1150dba3dabca7ab3e74cc642b737d6bacebfd
JuliOli-iu <olivera.julieta1912@gmail.com>
2024-11-08 22:39:55 +0000

    cambios :lipstick:

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,11 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light shadow-sm  px-4 mb-3">
 			<Link to="/">
-			<span className="p-2 navbar-brand mb-0 mr-2"> 📓 Agenda de ju 😸</span>
+			<span className="p-2 navbar-brand mb-0 mr-2" style={{fontFamily: "Ditty", fontSize: "30px"}}> 📓 進撃の巨人, Shingeki no Kyojin </span>
 			</Link>
 			<div className="ml-auto btn">
 				<Link to="/create">
-					<button className="btn btn-primary ">Create Contact</button>
+					<button className="btn btn-success ">Create Contact</button>
 				</Link>
 			</div>
 		</nav>
